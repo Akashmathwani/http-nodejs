b049c3d3ada0dae5daeb6fda273272cd9cb97515
Akash Mathwani <mathwaniakash@gmail.com>
2023-02-26 16:29:18 +0530

    bot fix

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const Telegraf = require("telegraf");
-const bot = new Telegraf("6095973837:AAEhSgjOZkoZEOGYDUo9RqgjlcsdUw522_Y");
-bot.start((ctx) => ctx.reply("Hello world"));
-
-bot.command("status", async (ctx) => {
-  try {
-    await ctx.reply(`Google service: ✅`);
-  } catch (error) {
-    console.error(error);
-  }
-});
+const { Telegraf } = require("telegraf");
 
 const startBot = async () => {
+  const bot = new Telegraf("6095973837:AAEhSgjOZkoZEOGYDUo9RqgjlcsdUw522_Y");
+  bot.start((ctx) => ctx.reply("Hello world"));
+
+  bot.command("status", async (ctx) => {
+    try {
+      await ctx.reply(`Google service: ✅`);
+    } catch (error) {
+      console.error(error);
+    }
+  });
   console.log("STARTING BOT");
   try {
     await bot.launch();
