c18e42387b546ea273b591589c84c5fd274fd553
Akash Mathwani <mathwaniakash@gmail.com>
2023-02-26 16:24:37 +0530

    node bot

---
+++ index.js
@@ -0,0 +1,24 @@
+const Telegraf = require("telegraf");
+const bot = new Telegraf("6095973837:AAEhSgjOZkoZEOGYDUo9RqgjlcsdUw522_Y");
+bot.start((ctx) => ctx.reply("Hello world"));
+
+bot.command("status", async (ctx) => {
+  try {
+    await ctx.reply(`Google service: ✅`);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const startBot = async () => {
+  console.log("STARTING BOT");
+  try {
+    await bot.launch();
+    console.log("Bot started successfully");
+  } catch (error) {
+    console.error(error);
+  }
+};
+console.log("RUNNING  SCRIPT ..");
+
+startBot();
